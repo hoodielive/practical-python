354278d75b9d8eabcfba5429fb76bac5aeb2d014
hoodielive <hoodielive@gmail.com>
2020-10-07 22:34:29 -0400

    Lets see if we can print out characters.

--- review/oop/with_strings.py
+++ review/oop/with_strings.py
@@ -9,3 +9,7 @@ class MapOdu(object):
 
     def __repr__self(self):
         return 'Date(%r,%r,%r)' % (self.first_leg, self.second_leg, self.interpretation)
+
+map_odu_1 = MapOdu('Osa', 'Okanran', 'An unexpected change leads to a new direction/opportunity.')
+
+print(map_odu_1.__repr__)
