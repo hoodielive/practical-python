d1b62d4f3d807a666b842d48aa42d72581b48f1f
hoodielive <hoodielive@gmail.com>
2020-10-07 22:32:29 -0400

    I added some items to try writing my own string conversions.

--- review/oop/with_strings.py
+++ review/oop/with_strings.py
@@ -1,7 +1,11 @@
-from datetime import date
+class MapOdu(object):
+    def __init__(self, first_leg, second_leg, interpretation):
+        self.first_leg = first_leg
+        self.second_leg = second_leg
+        self.interpretation = interpretation
 
-todays_date_is = date(2020, 10, 7)
+    def __str__(self):
+        return '%d-%d-%d' % (self.first_leg, self.second_leg, self.interpretation)
 
-print(todays_date_is)
-print(str(todays_date_is))
-print(repr(todays_date_is))
+    def __repr__self(self):
+        return 'Date(%r,%r,%r)' % (self.first_leg, self.second_leg, self.interpretation)
